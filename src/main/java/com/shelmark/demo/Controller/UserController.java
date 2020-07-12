404fee81ba3fecf1ef498c49d95801e615c52018
Jack Edison <sangtntgcs190019@gmail.com>
2020-07-12 15:10:26 +0700

    complete add user

--- src/main/java/com/shelmark/demo/Controller/UserController.java
+++ src/main/java/com/shelmark/demo/Controller/UserController.java
@@ -1,32 +1,38 @@
 package com.shelmark.demo.Controller;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.shelmark.demo.Entity.Permission;
 import com.shelmark.demo.Entity.User;
+import com.shelmark.demo.Service.ImageService;
 import com.shelmark.demo.Service.PermissionService;
 import com.shelmark.demo.Service.UserService;
 
 @Controller
 @RequestMapping("/admin/user")
 public class UserController {
-	@Autowired 
+	@Autowired
 	private UserService userService;
-	
-	@Autowired 
+
+	@Autowired
 	private PermissionService perService;
+
+	public static String uploadRootPath = System.getProperty("user.dir") + "/src/main/webapp/resources/static/img";
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public ModelAndView getUsers(@RequestParam(required = false) Integer page,
 			@RequestParam(required = false) Integer limit) {
@@ -43,20 +49,20 @@ public class UserController {
 		mv.addObject("users", users);
 		return mv;
 	}
-	
-	@RequestMapping(value="/decentralization", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/decentralization", method = RequestMethod.GET)
 	public String decentralize(@RequestParam String username) {
 		return "redirect:/admin/user";
 	}
-	
-	@RequestMapping(value="/deleteUser", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/deleteUser", method = RequestMethod.POST)
 	public String delete(@RequestParam String username) {
 		User user = userService.findByUsername(username);
 		userService.delete(user);
 		return "redirect:/admin/user";
 	}
-	
-	@RequestMapping(value="/addPermission", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/addPermission", method = RequestMethod.GET)
 	public ModelAndView addPer(@RequestParam String username) {
 		List<Permission> pers = perService.findAll();
 		ModelAndView mv = new ModelAndView();
@@ -65,12 +71,12 @@ public class UserController {
 		mv.addObject("username", username);
 		return mv;
 	}
-	
-	@RequestMapping(value="/addPermission", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/addPermission", method = RequestMethod.POST)
 	public String addPermission(@RequestParam String username, @RequestParam List<Long> per) {
 		User user = userService.findByUsername(username);
 		Set<Permission> newPers = user.getPermissions();
-		for (Long p: per) {
+		for (Long p : per) {
 			Permission permission = perService.findById(p);
 			newPers.add(permission);
 		}
@@ -78,7 +84,7 @@ public class UserController {
 		userService.save(user);
 		return "redirect:/admin/user";
 	}
-	
+
 	@RequestMapping(value = "/createUser", method = RequestMethod.GET)
 	public ModelAndView getNewUserView() {
 		User user = new User();
@@ -88,21 +94,40 @@ public class UserController {
 		return model;
 	}
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired 
+	private ImageService imgService;
 	@RequestMapping(value = "/createUser", method = RequestMethod.POST)
-	public String saveUser(@ModelAttribute("user") User userModel, HttpServletRequest request,
-			Model model) {
-		if (userModel.getUsername() == null) {
-			String errorMessage = userService.saveUser(userModel);
-			model.addAttribute("errorMessage", errorMessage);
-			if (errorMessage != null) {
-				return "createUser";
-			}
+	public String saveUser(@RequestParam String username, 
+							@RequestParam String password, 
+							@RequestParam String phone,
+							@RequestParam String email,
+							@RequestParam String gender,
+							@RequestParam String address,
+							@RequestParam MultipartFile file
+	) {
+		User user = new User();
+		user.setUsername(username);
+		user.setEmail(password);
+		user.setPassword(passwordEncoder.encode(password));
+		Date date = new Date();
+		user.setDate(date.getTime());
+		user.setEmail(email);
+		user.setGender(gender);
+		user.setAddress(address);
+		String img ="/resources/static/img/avatar/";
+		if (!file.isEmpty()) {
+			img += imgService.uploadFile(uploadRootPath + "/avatar", file);
 		} else {
-			userService.saveUser(userModel);
+			img += gender+".png";
 		}
+		user.setImage(img);
+		userService.save(user);
 		return "redirect:/admin/user";
 	}
-	
+
 	@RequestMapping(value = "/editUser", method = RequestMethod.GET)
 	public ModelAndView getEditView(@RequestParam("username") String username) {
 		User user = userService.getUsername(username);
@@ -113,8 +138,7 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/editUser", method = RequestMethod.POST)
-	public String editUser(@ModelAttribute("user") User userModel, HttpServletRequest request,
-			Model model) {
+	public String editUser(@ModelAttribute("user") User userModel, HttpServletRequest request, Model model) {
 		if (userModel.getUsername() == null) {
 			String errorMessage = userService.saveUser(userModel);
 			model.addAttribute("errorMessage", errorMessage);
--- src/main/webapp/WEB-INF/views/createUser.jsp
+++ src/main/webapp/WEB-INF/views/createUser.jsp
@@ -1,64 +1,45 @@
-<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<div class="container-fluid">
+	<form enctype="multipart/form-data" action="/admin/user/createUser" method="post">
+		<div class="form-group">
+			<label for="username">User Name</label> <input type="text"
+				class="form-control" name="username" />
+		</div>
 
-<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Create User</title>
-</head>
-<body>
-	<div class="container-fluid">
-		<form action="/admin/product/editPro" method="POST"
-			enctype="multipart/form-data">
-			<form:form enctype="multipart/form-data"
-				action="/admin/user/createUser" modelAttribute="user" method="post">
-				<form:input type="hidden" path="username" />
+		<div class="form-group">
+			<label for=password>Password</label> <input type="password"
+				class="form-control" name="password" />
+		</div>
 
-				<div class="form-group">
-					<label for="username">User Name</label>
-					<form:input type="text" class="form-control" path="username" />
-				</div>
+		<div class="form-group">
+			<label for="email">Email</label> <input type="text"
+				class="form-control" name="email" />
+		</div>
 
-				<div class="form-group">
-					<label for=password>Password</label>
-					<form:input type="text" class="form-control" path="password" />
-				</div>
+		<div class="form-group">
+			<label for="phone">Phone</label> <input type="text"
+				class="form-control" name="phone" />
+		</div>
 
-				<div class="form-group">
-					<label for="email">Email</label>
-					<form:input type="email" class="form-control" path="email" />
-				</div>
-
-				<div class="form-group">
-					<label for="phone">Phone</label>
-					<form:input type="text" class="form-control" path="phone" />
-				</div>
-
-				<div class="form-group">
-					<label for="gender">Gender</label>
-					<form:input type="text" class="form-control" path="gender" />
-				</div>
-
-				<div class="form-group">
-					<label for="balance">Balance</label>
-					<form:input type="text" class="form-control" path="balance" />
-				</div>
-
-
-
-				<div class="form-group">
-					<label for="address">Address</label>
-					<form:input type="text" class="form-control" path="address" />
-				</div>
-				<button type="submit" class="btn btn-primary">Finish</button>	
-				<a href="/admin/user" class="btn btn-danger">Cancel</a>
-			</form:form>
-		</form>
-	</div>
-
-</body>
+		<div class="form-group">
+			<label for="gender">Gender</label> <select class="form-control"
+				name="gender">
+				<option value="male">Male</option>
+				<option value="female">Female</option>
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="address">Address</label> <input type="text"
+				class="form-control" name="address" />
+		</div>
+		<div class="form-group">
+			<label>Image</label> <input name="file" id="myfile" type="file"
+				class="form-control">
+		</div>
+		<div class="form-group">
+			<img id="catImg" alt="Add avatar" src="" height="200px" />
+		</div>
+		<button type="submit" class="btn btn-primary">Finish</button>
+		<a href="/admin/user" class="btn btn-danger">Cancel</a>
+	</form>
+</div>
